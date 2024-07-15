bf76d0e35d5eff38945d63f2db32c7c8beb77f94
Morgan <mrgnhnt96@gmail.com>
2024-07-15 09:14:20 -0700

    feat: add date annotation for set header

--- packages/revali_router_annotations/lib/src/set_header.dart
+++ packages/revali_router_annotations/lib/src/set_header.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
+
 class SetHeader {
   const SetHeader(this.name, this.value);
+
+  /// The value of the header will be set to an empty string
+  ///
+  /// Useful for headers that don't have a value, or are dynamic
+  const SetHeader.key(this.name) : value = '';
+
+  /// the date header will be set at the time of the response
+  const SetHeader.date() : this(HttpHeaders.dateHeader, '');
 
   final String name;
   final String value;
