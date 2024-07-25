2e35afb11a955c574a550bd3247adbc53cb13c9f
Morgan <mrgnhnt96@gmail.com>
2024-07-25 08:49:10 -0700

    chore: update comment

--- packages/revali_router_core/lib/exception_catcher/exception_catcher.dart
+++ packages/revali_router_core/lib/exception_catcher/exception_catcher.dart
@@ -6,7 +6,7 @@ abstract class ExceptionCatcher<T extends Exception> {
   const ExceptionCatcher();
 
   bool canCatch(Object exception) {
-    // If T is Object then the ExceptionCatcher was not
+    // If T is `Exception` then the ExceptionCatcher was not
     // supplied with a type argument
     // ignore: literal_only_boolean_expressions
     if ('$T' == '$Exception') {
