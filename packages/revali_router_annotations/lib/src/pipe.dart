565010f738f863449dd75b9057776d7e8735304b
Morgan <mrgnhnt96@gmail.com>
2024-08-06 06:32:14 -0700

    refactor: remove override error response

--- packages/revali_router_annotations/lib/src/pipe.dart
+++ packages/revali_router_annotations/lib/src/pipe.dart
@@ -2,18 +2,8 @@ import 'dart:async';
 
 import 'package:revali_router_core/revali_router_core.dart';
 
-abstract class Pipe<T, R> extends OverrideErrorResponse
-    with OverrideErrorResponseMixin {
+abstract class Pipe<T, R> {
   const Pipe();
 
   FutureOr<R> transform(T value, PipeContext<dynamic> context);
-
-  @override
-  Object? get body => null;
-
-  @override
-  Map<String, String>? get headers => null;
-
-  @override
-  int? get statusCode => null;
 }
