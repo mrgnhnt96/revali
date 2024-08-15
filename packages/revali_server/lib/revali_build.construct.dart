cf167c935862cc51ebce15185e4d5816e37498e1
Morgan <mrgnhnt96@gmail.com>
2024-08-15 11:18:14 -0700

    refactor: remove pre build throw exception

--- packages/revali_server/lib/revali_build.construct.dart
+++ packages/revali_server/lib/revali_build.construct.dart
@@ -1,14 +1,8 @@
 import 'package:revali_construct/models/files/any_file.dart';
-import 'package:revali_construct/models/revali_build_context.dart';
 import 'package:revali_construct/revali_construct.dart';
 
 class RevaliBuildConstruct extends BuildConstruct {
   const RevaliBuildConstruct();
-
-  @override
-  Future<void> preBuild(RevaliBuildContext context, MetaServer server) {
-    throw UnimplementedError();
-  }
 
   @override
   BuildDirectory generate(RevaliContext context, MetaServer server) {
