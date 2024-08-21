8be618929cb1e414417829f8c5b141a98eb39aaf
Morgan <mrgnhnt96@gmail.com>
2024-08-21 09:07:20 -0700

    feat: support chunking

--- packages/revali_router/lib/utils/http_response_extensions.dart
+++ packages/revali_router/lib/utils/http_response_extensions.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:revali_router/src/body/response_body/base_body_data.dart';
-import 'package:revali_router/src/payload/payload_impl.dart';
 import 'package:revali_router/src/response/mutable_response_impl.dart';
 import 'package:revali_router/src/response/web_socket_response.dart';
 import 'package:revali_router_core/revali_router_core.dart';
@@ -29,9 +28,7 @@ extension HttpResponseX on HttpResponse {
       responseHeaders = mutableResponse.headersToSend;
     }
 
-    responseHeaders
-      ..contentLength ??= 0
-      ..mimeType ??= 'text/plain';
+    responseHeaders.mimeType ??= 'text/plain';
     http.statusCode = response.statusCode;
 
     switch (response.statusCode) {
@@ -44,26 +41,26 @@ extension HttpResponseX on HttpResponse {
         break;
     }
 
-    var chunk = false;
-
-    if (responseHeaders[HttpHeaders.transferEncodingHeader]
-        case final coding?) {
-      if (!equalsIgnoreAsciiCase(coding, 'identity')) {
+    var useChunking = false;
+    var deChunkBeforeSending = false;
+    if (responseHeaders.transferEncoding case final transfer?) {
+      if (!equalsIgnoreAsciiCase(transfer, 'identity')) {
         // If the response is already in a chunked encoding, de-chunk it because
         // otherwise `dart:io` will try to add another layer of chunking.
-        chunk = true;
-
-        responseHeaders.set(HttpHeaders.transferEncodingHeader, 'chunked');
-      } else if (response.statusCode >= 200 &&
-          response.statusCode != 204 &&
-          response.statusCode != 304 &&
-          responseHeaders.contentLength == null &&
-          responseHeaders.mimeType != 'multipart/byteranges') {
-        // If the response isn't chunked yet and
-        // there's no other way to tell its
-        // length, enable `dart:io`'s chunked encoding.
-        responseHeaders.set(HttpHeaders.transferEncodingHeader, 'chunked');
+        deChunkBeforeSending = true;
+        useChunking = true;
       }
+    } else if (response.statusCode >= 200 &&
+        response.statusCode != 204 &&
+        response.statusCode != 304 &&
+        responseHeaders.contentLength == null &&
+        responseHeaders.mimeType != 'multipart/byteranges') {
+      // If the response isn't chunked yet and
+      // there's no other way to tell its
+      // length, enable `dart:io`'s chunked encoding.
+      useChunking = true;
+    } else if (responseHeaders.contentLength == null) {
+      responseHeaders.contentLength = 0;
     }
 
     if (!responseHeaders.keys.contains(HttpHeaders.dateHeader)) {
@@ -80,40 +77,48 @@ extension HttpResponseX on HttpResponse {
       'OPTIONS',
     };
 
-    if (!disallowedMethods.contains(requestMethod) &&
-        !disallowedStatuses.contains(response.statusCode)) {
-      Stream<List<int>>? body;
-      if (response.body case final responseBody? when !responseBody.isNull) {
-        if (responseHeaders.range case final range?
-            when responseBody is FileBodyData) {
-          final (start, end) = range;
-          body = responseBody.range(start, end);
-        } else {
-          body = responseBody.read();
-        }
-      }
+    if (disallowedMethods.contains(requestMethod) &&
+        disallowedStatuses.contains(response.statusCode)) {
+      await _complete();
 
-      if (chunk && body != null) {
-        final payload = PayloadImpl(
-          response.body?.read(),
-          encoding: responseHeaders.encoding,
-        );
-        body = chunkedCoding.decoder.bind(payload.read());
+      return;
+    }
 
-        responseHeaders.contentLength = payload.contentLength ?? 0;
-      }
-
-      responseHeaders.forEach((key, values) {
-        http.headers.set(key, values.join(','));
-      });
-
-      if (body != null) {
-        await http.addStream(body);
+    Stream<List<int>>? body;
+    if (response.body case final responseBody? when !responseBody.isNull) {
+      if (responseHeaders.range case final range?
+          when responseBody is FileBodyData) {
+        final (start, end) = range;
+        body = responseBody.range(start, end);
+      } else {
+        body = responseBody.read();
       }
     }
 
-    await http.flush();
-    await http.close();
+    if (useChunking && body != null) {
+      responseHeaders.transferEncoding = 'chunked';
+
+      if (deChunkBeforeSending) {
+        body = chunkedCoding.decoder.bind(body);
+      }
+
+      body = chunkedCoding.encoder.bind(body);
+    }
+
+    responseHeaders.forEach((key, values) {
+      http.headers.set(key, values.join(','));
+    });
+
+    if (body != null) {
+      await http.addStream(body);
+    }
+
+    await _complete();
+  }
+
+  Future<void> _complete() async {
+    await flush();
+    await close();
   }
 }
 
