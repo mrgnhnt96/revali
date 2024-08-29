99bbbaa925537748f04f6e083b40f002086b8cf5
Jonah Walker <supposedlysam@gmail.com>
2024-08-29 16:36:20 -0400

    feat: create docs only site at root

--- doc-site/docusaurus.config.ts
+++ doc-site/docusaurus.config.ts
@@ -34,27 +34,14 @@ const config: Config = {
             'classic',
             {
                 docs: {
+                    routeBasePath: '/',
                     sidebarPath: './sidebars.ts',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
                         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
-                blog: {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                },
+                blog: false,
                 theme: {
                     customCss: './src/css/custom.css',
                 },
@@ -78,7 +65,6 @@ const config: Config = {
                     position: 'left',
                     label: 'Tutorial',
                 },
-                { to: '/blog', label: 'Blog', position: 'left' },
                 {
                     href: 'https://github.com/facebook/docusaurus',
                     label: 'GitHub',
@@ -94,7 +80,7 @@ const config: Config = {
                     items: [
                         {
                             label: 'Tutorial',
-                            to: '/docs/intro',
+                            to: '/intro',
                         },
                     ],
                 },
@@ -118,10 +104,6 @@ const config: Config = {
                 {
                     title: 'More',
                     items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
                         {
                             label: 'GitHub',
                             href: 'https://github.com/facebook/docusaurus',
