bbb2c9f261aae91f5f23f9fa2ef70e04f6449fae
Morgan <mrgnhnt96@gmail.com>
2024-10-03 00:18:00 -0600

    feat: add class modifier interface to guard

--- packages/revali_router_core/lib/guard/guard.dart
+++ packages/revali_router_core/lib/guard/guard.dart
@@ -2,7 +2,7 @@ import 'package:revali_router_core/guard/guard_action.dart';
 import 'package:revali_router_core/guard/guard_context.dart';
 import 'package:revali_router_core/guard/guard_result.dart';
 
-abstract class Guard {
+abstract interface class Guard {
   const Guard();
 
   Future<GuardResult> canActivate(
