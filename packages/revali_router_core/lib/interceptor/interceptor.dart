39d07ee6d969e423e8d8ed2d2696904bbc667064
Morgan <mrgnhnt96@gmail.com>
2024-10-16 10:12:50 -0700

    feat: update types on interceptors to be restrictive on pre and full on post

--- packages/revali_router_core/lib/interceptor/interceptor.dart
+++ packages/revali_router_core/lib/interceptor/interceptor.dart
@@ -3,7 +3,7 @@ import 'package:revali_router_core/interceptor/interceptor_context.dart';
 abstract interface class Interceptor {
   const Interceptor();
 
-  Future<void> pre(InterceptorContext context);
+  Future<void> pre(RestrictedInterceptorContext context);
 
-  Future<void> post(InterceptorContext context);
+  Future<void> post(FullInterceptorContext context);
 }
--- packages/revali_router_core/lib/interceptor/interceptor_context.dart
+++ packages/revali_router_core/lib/interceptor/interceptor_context.dart
@@ -3,13 +3,25 @@ import 'package:revali_router_core/interceptor/interceptor_meta.dart';
 import 'package:revali_router_core/reflect/read_only_reflect_handler.dart';
 import 'package:revali_router_core/request/mutable_request.dart';
 import 'package:revali_router_core/response/mutable_response.dart';
+import 'package:revali_router_core/response/read_only_response.dart';
+import 'package:revali_router_core/response/restricted_mutable_response.dart';
 
-abstract class InterceptorContext {
+abstract class InterceptorContext<T extends ReadOnlyResponse> {
   const InterceptorContext();
 
   InterceptorMeta get meta;
   DataHandler get data;
   MutableRequest get request;
-  MutableResponse get response;
+  T get response;
   ReadOnlyReflectHandler get reflect;
+}
+
+abstract class RestrictedInterceptorContext
+    extends InterceptorContext<RestrictedMutableResponse> {
+  const RestrictedInterceptorContext();
+}
+
+abstract class FullInterceptorContext
+    extends InterceptorContext<MutableResponse> {
+  const FullInterceptorContext();
 }
