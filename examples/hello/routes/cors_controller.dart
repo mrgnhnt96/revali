6a9a74956dc88b47e5d267b1d463a90b9eb508ad
Morgan <mrgnhnt96@gmail.com>
2024-11-21 09:49:31 -0700

    feat: update example with multiple allow headers

--- examples/hello/routes/cors_controller.dart
+++ examples/hello/routes/cors_controller.dart
@@ -2,10 +2,14 @@ import 'package:revali_router/revali_router.dart';
 
 @ExpectHeaders({'X-Some-Header'})
 @AllowHeaders.simple()
+@AllowHeaders({'X-Different-Header'})
 @Controller('cors')
 class CorsController {
   const CorsController();
 
   @Get()
+  @AllowHeaders.noInherit({'X-Another-Header'})
+  @AllowHeaders.simple()
+  @AllowHeaders.common()
   void headers() {}
 }
