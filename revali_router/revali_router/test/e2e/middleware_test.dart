a96e8521094b73cf8e44db83805b2b339d152013
Morgan <mrgnhnt96@gmail.com>
2024-11-25 09:14:20 -0700

    feat: finish tests for middleware

--- revali_router/revali_router/test/e2e/middleware_test.dart
+++ revali_router/revali_router/test/e2e/middleware_test.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:revali_router/revali_router.dart';
 import 'package:test/test.dart';
@@ -8,45 +8,115 @@ import 'utils/test_request.dart';
 void main() {
   group(Middleware, () {
     test('runs gracefully', () async {
-      final router = Router(
-        routes: [
-          Route(
-            '',
-            middlewares: const [_SuccessMiddleware()],
-            method: 'GET',
-            handler: (context) async {},
-          ),
-        ],
-      );
+      final middleware = _SuccessMiddleware();
 
       await testRequest(
-        router,
-        method: 'GET',
-        path: '',
+        TestRoute(
+          middlewares: [middleware],
+        ),
         verifyResponse: (response, context) {
-          expect(response.statusCode, HttpStatus.ok);
-          expect(response.body, isA<MutableBody>());
-          expect(response.body?.isNull, isTrue);
-          final headers = response.joinedHeaders.values;
+          expect(middleware.wasCalled, isTrue);
+        },
+      );
+    });
 
-          expect(headers, hasLength(4));
+    test('runs in order', () async {
+      final controller = StreamController<int>();
 
-          expect(
-            headers[HttpHeaders.accessControlAllowMethodsHeader]?.single,
-            'OPTIONS, GET, HEAD',
-          );
-          expect(
-            headers[HttpHeaders.allowHeader]?.single,
-            'OPTIONS, GET, HEAD',
-          );
-          expect(
-            headers[HttpHeaders.accessControlAllowCredentialsHeader]?.single,
-            'true',
-          );
-          expect(
-            headers[HttpHeaders.accessControlAllowOriginHeader]?.single,
-            '*',
-          );
+      await testRequest(
+        TestRoute(
+          middlewares: List.generate(
+            5,
+            (index) => _OrderedMiddleware(index, controller),
+          ),
+        ),
+        verifyResponse: (response, context) {
+          expect(controller.stream, emitsInOrder([0, 1, 2, 3, 4]));
+        },
+      );
+    });
+
+    requestTest(
+      'modifications persist throughout request',
+      TestRoute(
+        middlewares: [_ModifyingMiddleware()],
+        handler: (context) async {
+          expect(context.data.contains('Hello, World!'), isTrue);
+        },
+      ),
+      verifyResponse: (response, context) {
+        expect(response.body?.data, 'Hello, World!');
+        expect(response.headers['hello'], 'world');
+        expect(context.request.headers['hello'], 'world');
+      },
+    );
+
+    requestTest(
+      'modifications can be overridden',
+      TestRoute(
+        middlewares: [_ModifyingMiddleware()],
+        handler: (context) async {
+          context.data.remove<String>();
+
+          context.response.headers.remove('hello');
+          context.request.headers.remove('hello');
+
+          context.response.body = 'Goodbye, World!';
+
+          context.response.headers['goodbye'] = 'world';
+          context.request.headers['goodbye'] = 'world';
+
+          expect(context.data.contains('Hello, World!'), isFalse);
+          expect(context.data.has<String>(), isFalse);
+        },
+      ),
+      verifyResponse: (response, context) {
+        expect(response.body?.data, 'Goodbye, World!');
+
+        expect(response.headers['hello'], isNull);
+        expect(context.request.headers['hello'], isNull);
+
+        expect(response.headers['goodbye'], 'world');
+        expect(context.request.headers['goodbye'], 'world');
+      },
+    );
+
+    requestTest(
+      'returning a response halts execution and overrides response',
+      TestRoute(
+        middlewares: const [_HaltingMiddleware.overrideResponse()],
+      ),
+      verifyResponse: (response, context) {
+        expect(response.body?.data, 'Overridden');
+        expect(response.headers['overridden'], 'true');
+        expect(response.statusCode, -1);
+      },
+    );
+
+    requestTest(
+      'returning a response halts execution and inherits existing response',
+      TestRoute(
+        middlewares: const [_HaltingMiddleware()],
+      ),
+      verifyResponse: (response, context) {
+        expect(response.body?.data, 'Not overridden');
+        expect(response.headers['overridden'], 'false');
+        expect(response.statusCode, 400);
+      },
+    );
+
+    test('does not execute subsequent middleware if halted', () async {
+      final successMiddleware = _SuccessMiddleware();
+
+      await testRequest(
+        TestRoute(
+          middlewares: [
+            const _HaltingMiddleware(),
+            successMiddleware,
+          ],
+        ),
+        verifyResponse: (response, context) {
+          expect(successMiddleware.wasCalled, isFalse);
         },
       );
     });
@@ -54,12 +124,74 @@ void main() {
 }
 
 class _SuccessMiddleware implements Middleware {
-  const _SuccessMiddleware();
+  _SuccessMiddleware();
+
+  bool wasCalled = false;
+
   @override
   Future<MiddlewareResult> use(
     MiddlewareContext context,
     MiddlewareAction action,
   ) async {
+    wasCalled = true;
     return action.next();
+  }
+}
+
+class _OrderedMiddleware implements Middleware {
+  _OrderedMiddleware(this.index, this.controller);
+
+  final int index;
+  final StreamController<int> controller;
+
+  @override
+  Future<MiddlewareResult> use(
+    MiddlewareContext context,
+    MiddlewareAction action,
+  ) async {
+    controller.add(index);
+    return action.next();
+  }
+}
+
+class _ModifyingMiddleware implements Middleware {
+  _ModifyingMiddleware();
+
+  @override
+  Future<MiddlewareResult> use(
+    MiddlewareContext context,
+    MiddlewareAction action,
+  ) async {
+    context.data.add('Hello, World!');
+    context.response.body = 'Hello, World!';
+    context.response.headers['hello'] = 'world';
+    context.request.headers['hello'] = 'world';
+
+    return action.next();
+  }
+}
+
+class _HaltingMiddleware implements Middleware {
+  const _HaltingMiddleware() : overrideResponse = false;
+  const _HaltingMiddleware.overrideResponse() : overrideResponse = true;
+
+  final bool overrideResponse;
+
+  @override
+  Future<MiddlewareResult> use(
+    MiddlewareContext context,
+    MiddlewareAction action,
+  ) async {
+    if (overrideResponse) {
+      return action.stop(
+        body: 'Overridden',
+        headers: {'overridden': 'true'},
+        statusCode: -1,
+      );
+    }
+
+    context.response.body = 'Not overridden';
+    context.response.headers['overridden'] = 'false';
+    return action.stop();
   }
 }
