06278f877d9e92885ece30eb4f0cd074388764ce
Morgan <mrgnhnt96@gmail.com>
2025-02-20 08:19:40 -0700

    feat: get dependencies dynamically

--- constructs/server_client/server_client_gen/lib/makers/files/pubspec_file.dart
+++ constructs/server_client/server_client_gen/lib/makers/files/pubspec_file.dart
@@ -6,59 +6,67 @@ import 'package:server_client_gen/models/client_server.dart';
 import 'package:server_client_gen/models/settings.dart';
 
 AnyFile pubspecFile(ClientServer server, Settings settings) {
-  final serverClientSegments = Isolate.resolvePackageUriSync(
+  final serverClient = Isolate.resolvePackageUriSync(
     Uri.parse('package:server_client/'),
-  ).toString().split(p.separator);
+  ).toString();
+
+  final imports = server.controllers
+      .expand(
+        (e) => e.methods.expand(
+          (e) => e.returnType.imports.expand(
+            (e) => e?.packages.toList() ?? <String>[],
+          ),
+        ),
+      )
+      .toSet();
+
+  final packages = <(String, String)>[('server_client', serverClient)];
+  for (final import in imports) {
+    final [_, String packagePath] = import.split(':');
+    final [String package, ...] = packagePath.split('/');
+
+    final resolved = Isolate.resolvePackageUriSync(
+      Uri.parse('package:$package/'),
+    ).toString();
+
+    packages.add((package, resolved));
+  }
 
   final dependencies = StringBuffer();
 
-  for (final dep in settings.dependencies) {
-    var dependency = dep;
-    if (!dep.contains(': ')) {
-      dependency += ': any';
-    }
-
-    dependency = dependency.replaceAll('\n', '\n  ').trim();
-    dependencies.writeln('''
-  $dependency
-''');
-  }
-
   switch (server.hasWebsockets) {
     case true:
-      dependencies.writeln('''
-  web_socket_channel:
-''');
+      dependencies.writeln('  web_socket_channel: ^3.0.2');
 
     case false:
       break;
   }
 
-  switch ((
-    serverClientSegments.contains('hosted'),
-    serverClientSegments.reversed.toList()..removeWhere((e) => e.isEmpty),
-  )) {
-    case (true, [_, final String package, ...]):
-      final version = package.replaceAll('server_client-', '');
+  for (final (package, path) in packages) {
+    final segments = path.split('/');
+    switch ((
+      segments.contains('hosted'),
+      segments.reversed.toList()..removeWhere((e) => e.isEmpty),
+    )) {
+      case (true, [_, final String package, ...]):
+        final version = package.replaceAll('server_client-', '');
 
-      dependencies.writeln('''
-  server_client: $version
-''');
-    case (false, _):
-      Iterable<String> clean() sync* {
-        for (final part in serverClientSegments.skip(1)) {
-          if (part.isEmpty) continue;
-          if (part == 'lib') continue;
-          yield part;
+        dependencies.writeln('  $package: $version');
+      case (false, _):
+        Iterable<String> clean() sync* {
+          for (final part in segments.skip(1)) {
+            if (part.isEmpty) continue;
+            if (part == 'lib') continue;
+            yield part;
+          }
         }
-      }
 
-      final path = p.joinAll([p.separator, ...clean()]);
+        final path = p.joinAll([p.separator, ...clean()]);
 
-      dependencies.writeln('''
-  server_client:
-    path: $path
-''');
+        dependencies.writeln('''
+  $package:
+    path: $path''');
+    }
   }
 
   return AnyFile(
--- constructs/server_client/server_client_gen/lib/models/settings.dart
+++ constructs/server_client/server_client_gen/lib/models/settings.dart
@@ -1,20 +1,13 @@
 class Settings {
   const Settings({
     required this.packageName,
-    required this.dependencies,
   });
 
   factory Settings.fromJson(Map<dynamic, dynamic> json) {
     return Settings(
       packageName: json['package_name'] as String? ?? 'client',
-      dependencies: switch (json['dependencies']) {
-        final List<dynamic> deps => deps.map((dep) => '${dep ?? ''}').toList(),
-        _ => <String>[],
-      }
-        ..removeWhere((e) => e.isEmpty),
     );
   }
 
   final String packageName;
-  final List<String> dependencies;
 }
