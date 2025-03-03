5a8bd89b31315a7bd3a21394489de5117e4a5462
Morgan <mrgnhnt96@gmail.com>
2025-03-03 08:59:26 -0700

    fix: change type from void to future void

--- revali_router/revali_router_core/lib/body/mutable_body.dart
+++ revali_router/revali_router_core/lib/body/mutable_body.dart
@@ -2,7 +2,7 @@ import 'package:revali_router_core/body/body_data.dart';
 
 abstract base class MutableBody extends BodyData {
   /// Replaces the entire body with the given data.
-  void replace(Object? data);
+  Future<void> replace(Object? data);
 
   /// Adds/Replaces a single key in the body with the given data.
   void operator []=(String key, Object? data);
