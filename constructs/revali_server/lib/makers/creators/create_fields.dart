16c200a270195bf5a4f578ee0d939ce472fbf9f9
Morgan <mrgnhnt96@gmail.com>
2025-03-11 08:49:23 -0700

    fix: bad AI, should get all args

--- constructs/revali_server/lib/makers/creators/create_fields.dart
+++ constructs/revali_server/lib/makers/creators/create_fields.dart
@@ -3,11 +3,11 @@ import 'package:revali_server/utils/annotation_arguments.dart';
 
 List<Field> createFields(AnnotationArguments arguments) {
   Iterable<Field> iterate() sync* {
-    for (final arg in arguments.positional) {
+    for (final MapEntry(key: name, value: arg) in arguments.all.entries) {
       yield Field(
         (p) => p
           ..type = refer(arg.type.name)
-          ..name = arg.parameterName
+          ..name = name
           ..modifier = FieldModifier.final$,
       );
     }
