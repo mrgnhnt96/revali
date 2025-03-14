5049d0456ce53664d31b8369529a4fc714d963b0
Morgan <mrgnhnt96@gmail.com>
2025-03-14 12:59:26 -0700

    feat: create meta record prop

---
+++ packages/revali_construct/lib/models/meta_record_prop.dart
@@ -0,0 +1,38 @@
+import 'package:analyzer/dart/element/type.dart';
+import 'package:revali_construct/models/meta_type.dart';
+
+class MetaRecordProp {
+  const MetaRecordProp({
+    required this.name,
+    required this.isNamed,
+    required this.type,
+  });
+
+  static Iterable<MetaRecordProp> fromRecordType(RecordType type) sync* {
+    for (final field in type.positionalFields) {
+      final type = MetaType.fromType(field.type);
+
+      yield MetaRecordProp(
+        name: null,
+        isNamed: false,
+        type: type,
+      );
+    }
+
+    for (final field in type.namedFields) {
+      final type = MetaType.fromType(field.type);
+
+      yield MetaRecordProp(
+        name: field.name,
+        isNamed: true,
+        type: type,
+      );
+    }
+  }
+
+  final String? name;
+  final bool isNamed;
+  final MetaType type;
+
+  bool get isPositioned => !isNamed;
+}
