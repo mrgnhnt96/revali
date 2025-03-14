e22af30dcf340ebbcff07b1b8cf16dff60c71269
Morgan <mrgnhnt96@gmail.com>
2025-03-14 12:59:05 -0700

    feat: create factory from type

--- packages/revali_construct/lib/models/meta_param.dart
+++ packages/revali_construct/lib/models/meta_param.dart
@@ -1,11 +1,12 @@
+import 'package:analyzer/dart/element/element.dart';
 import 'package:revali_construct/models/meta_type.dart';
 import 'package:revali_construct/types/annotation_getter.dart';
+import 'package:revali_construct/utils/annotation_getter_impl.dart';
 
 class MetaParam {
   const MetaParam({
     required this.name,
     required this.type,
-    required this.nullable,
     required this.isRequired,
     required this.isNamed,
     required this.defaultValue,
@@ -13,17 +14,33 @@ class MetaParam {
     required this.literalValue,
   });
 
+  factory MetaParam.fromParam(ParameterElement param) {
+    return MetaParam(
+      name: param.name,
+      type: MetaType.fromType(param.type),
+      literalValue: null,
+      isRequired: param.isRequired,
+      isNamed: param.isNamed,
+      defaultValue: param.defaultValueCode,
+      annotationsFor: ({
+        required List<OnMatch> onMatch,
+        NonMatch? onNonMatch,
+      }) =>
+          getAnnotations(
+        element: param,
+        onMatch: onMatch,
+        onNonMatch: onNonMatch,
+      ),
+    );
+  }
+
   final String name;
   final MetaType type;
-  final bool nullable;
   final bool isRequired;
   final bool isNamed;
   final String? defaultValue;
   final AnnotationMapper annotationsFor;
   final String? literalValue;
-
-  @Deprecated('Use type.importPath')
-  String? get typeImport => type.importPath;
 
   bool get hasDefaultValue => defaultValue != null;
 }
