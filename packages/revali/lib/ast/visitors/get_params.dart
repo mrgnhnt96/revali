aa2b73de37f669269d010530084a545fd5db0281
Morgan <mrgnhnt96@gmail.com>
2025-03-14 12:58:38 -0700

    feat: use from type factory for type

--- packages/revali/lib/ast/visitors/get_params.dart
+++ packages/revali/lib/ast/visitors/get_params.dart
@@ -1,36 +1,15 @@
 import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/nullability_suffix.dart';
-import 'package:revali/utils/extensions/class_element_extensions.dart';
-import 'package:revali/utils/extensions/element_extensions.dart';
 import 'package:revali_construct/revali_construct.dart';
 
 Iterable<MetaParam> getParams(FunctionTypedElement element) {
   final params = <MetaParam>[];
 
   for (final param in element.parameters) {
-    final type = param.type.getDisplayString();
-
-    final element = param.type.element;
-    if (element == null) {
-      throw Exception('Element not found for type $type');
-    }
-
-    final hasFromJsonConstructor = switch (element) {
-      final ClassElement element => element.hasFromJsonMember,
-      _ => false,
-    };
-
     params.add(
       MetaParam(
         name: param.name,
-        type: MetaType(
-          name: type,
-          hasFromJsonConstructor: hasFromJsonConstructor,
-          importPath: element.importPath,
-          element: element,
-        ),
+        type: MetaType.fromType(param.type),
         literalValue: null,
-        nullable: param.type.nullabilitySuffix != NullabilitySuffix.none,
         isRequired: param.isRequired,
         isNamed: param.isNamed,
         defaultValue: param.defaultValueCode,
