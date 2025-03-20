e07d2dfb8e4a43b794ab370fb5b442548ad5a3a1
Morgan <mrgnhnt96@gmail.com>
2025-03-20 08:12:39 -0700

    feat: simplify should nest logic and recursively solve for stream

--- constructs/revali_server/lib/makers/creators/should_nest_json_in_data.dart
+++ constructs/revali_server/lib/makers/creators/should_nest_json_in_data.dart
@@ -1,40 +1,12 @@
-import 'package:revali_construct/revali_construct.dart';
 import 'package:revali_server/converters/server_type.dart';
 
 bool shouldNestJsonInData(ServerType returnType) {
-  if (returnType.isStream) {
-    return false;
-  }
-
   final type = switch (returnType) {
-    ServerType(isFuture: true, typeArguments: [final type]) => type,
+    ServerType(typeArguments: [final type]) => type,
     _ => returnType,
   };
 
-  // List<int>
-  if (type
-      case ServerType(
-        iterableType: IterableType.list,
-        typeArguments: [
-          ServerType(name: 'int'),
-        ],
-      )) {
-    return false;
-  }
-
-  // List<List<int>>
-  if (type
-      case ServerType(
-        iterableType: IterableType.list,
-        typeArguments: [
-          ServerType(
-            iterableType: IterableType.list,
-            typeArguments: [
-              ServerType(isPrimitive: true),
-            ],
-          ),
-        ],
-      )) {
+  if (type case ServerType(isBytes: true)) {
     return false;
   }
 
