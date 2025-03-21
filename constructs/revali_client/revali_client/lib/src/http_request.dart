26affa90ba12353400e5dcbb5750e7ad95639bb6
Morgan <mrgnhnt96@gmail.com>
2025-03-21 11:12:39 -0700

    feat: set body to string by default

--- constructs/revali_client/revali_client/lib/src/http_request.dart
+++ constructs/revali_client/revali_client/lib/src/http_request.dart
@@ -5,7 +5,7 @@ class HttpRequest {
     required this.method,
     required this.url,
     Map<String, String>? headers,
-    this.body,
+    this.body = '',
     this.bodyBytes,
     this.encoding,
     this.contentLength,
@@ -14,7 +14,7 @@ class HttpRequest {
   final String method;
   final Uri url;
   Map<String, String> headers;
-  String? body;
+  String body;
   List<int>? bodyBytes;
   Encoding? encoding;
   int? contentLength;
