693704471d07361204cb42dee850155dd5fc54af
Morgan <mrgnhnt96@gmail.com>
2025-03-21 08:11:23 -0700

    chore: clean up by reducing required resources

--- constructs/revali_client/revali_client_gen/lib/makers/creators/get_path_params.dart
+++ constructs/revali_client/revali_client_gen/lib/makers/creators/get_path_params.dart
@@ -2,15 +2,14 @@
 
 import 'package:code_builder/code_builder.dart';
 import 'package:revali_client_gen/makers/utils/type_extensions.dart';
-import 'package:revali_client_gen/models/client_method.dart';
 
 // The parameters contained within the path of the method
 //
 // e.g. /shop/:id/item/:itemId
 //
 // The path params are :id and :itemId
-Iterable<Parameter> getPathParams(ClientMethod method) sync* {
-  for (final param in method.params) {
+Iterable<Parameter> getPathParams(List<String> params) sync* {
+  for (final param in params) {
     yield Parameter(
       (b) => b
         ..name = param
